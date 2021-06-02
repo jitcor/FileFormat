976b9b199dbe8c82399f22d5983d8f0e27941888
ihbing <shilulu@forensix.cn>
2021-06-02 12:33:25 +0800

    Nothing

--- main.go
+++ main.go
@@ -9,42 +9,45 @@ import (
 	"os"
 )
 
-var FileFormatMap =make(map[string][]byte)
+var FileFormatMap = make(map[string][]byte)
+
+const VERSION = "1.0.0"
 func main() {
 	initFileFormatMap()
-	if len(os.Args)<=1{
+	if len(os.Args) <= 1 {
 		log.Fatal("Please input file path")
 		return
 	}
-		fName:=os.Args[1]
-	if data,err:=ioutil.ReadFile(fName);err!=nil{
+	fName:=os.Args[1]
+	if fName==""{
+		log.Fatal("Please input file path")
+		return
+	}
+	if data, err := ioutil.ReadFile(fName); err != nil {
 		log.Fatal(err)
-	}else {
-		for k,v:=range FileFormatMap{
-			if n:=bytes.Index(data,v);n>-1{
-				log.Fatal("FileFormat: ",fName,": ",k)
+	} else {
+		for k, v := range FileFormatMap {
+			if n := bytes.Index(data, v); n > -1 {
+				log.Fatal("FileFormat: ", fName, ": ", k)
 				return
 			}
 		}
-		log.Fatal("FileFormat: ",fName,": file format not recognized")
+		log.Fatal("FileFormat: ", fName, ": file format not recognized")
 	}
 }
 
 func initFileFormatMap() {
-	if cfg, err := ini.Load("format_list.ini");err!=nil{
+	if cfg, err := ini.Load("format_list.ini"); err != nil {
 		log.Fatal(err)
-	}else {
+	} else {
 		keys := cfg.Section("").Keys()
-		for i:=0;i< len(keys);i++{
-			if value,err:=hex.DecodeString(keys[i].Value());err!=nil{
+		for i := 0; i < len(keys); i++ {
+			if value, err := hex.DecodeString(keys[i].Value()); err != nil {
 				log.Fatal(err)
-			}else {
-				FileFormatMap[keys[i].Name()]=value
+			} else {
+				FileFormatMap[keys[i].Name()] = value
 			}
 		}
 	}
 
 }
-
-
-
